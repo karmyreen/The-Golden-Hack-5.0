e3c3642e8fa9807bdbc5d982c539030f0a55fb82
August R.M <sjsaug@gmail.com>
2023-10-07 17:12:58 -0400

    Co-authored-by: Zakariya <ZAKnCHEESE@users.noreply.github.com>

--- ai.py
+++ ai.py
@@ -9,7 +9,7 @@ completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a video game character giving tips on how to improve personal hygiene."},
-    {"role": "user", "content": "Give the user 3 short tips in point form on how to improve personal hygiene."},
+    {"role": "user", "content": "Give the user 3 short tips in point form on how to improve personal hygiene. Don't include backslash n for new lines in the answer."},
   ]
 )
 
