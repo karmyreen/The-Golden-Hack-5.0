10a1b24c950fc2ecaa3f2a9cb3641e9fa9c85d45
August R.M <sjsaug@gmail.com>
2023-10-07 16:10:42 -0400

    add openai key

--- ai.py
+++ ai.py
@@ -1,12 +1,16 @@
 import os
 import openai
+from dotenv import load_dotenv
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[],
-  temperature=0.8,
-  max_tokens=1024
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a video game character giving tips on how to improve personal hygiene."},
+    {"role": "user", "content": "Give the user 3 tips on how to improve personal hygiene."},
+  ]
 )
 
+print(completion.choices[0].message)
--- levels.py
+++ levels.py
@@ -1,3 +1,12 @@
 import pygame
 from settings import *
 
+class Level():
+    def __init__(self):
+
+        self.display = pygame.display.get_surface()
+        self.visible = pygame.sprite.Group()
+        self.obstacles = pygame.sprite.Group()
+
+    def run(self):
+        pass
