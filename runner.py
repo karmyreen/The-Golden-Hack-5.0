5984ff2d9f2c31ac2fea63faa6174cb42355dfed
August R.M <sjsaug@gmail.com>
2023-10-07 18:34:54 -0400

    runner

--- runner.py
+++ runner.py
@@ -10,13 +10,36 @@ class Runner(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        frame1 = pygame.image.load("visuals\\mario run\\mariorun1.png").convert_alpha()
-        frame1 = rotate_flip(frame1)
+        frame1 = pygame.image.load("visuals/player_animation/player_run_0.png").convert_alpha()
+        
+        frame2 = pygame.image.load("visuals/player_animation/player_run_1.png").convert_alpha()
 
-        frame2 = pygame.image.load("visuals\\mario run\\mariorun2.png").convert_alpha()
-        frame2 = rotate_flip(frame2)
+        frame3 = pygame.image.load("visuals/player_animation/player_run_2.png").convert_alpha()
 
-        frame3 = pygame.image.load("visuals\\mario run\\mariorun3.png").convert_alpha()
-        frame3 = rotate_flip(frame3)
+        frame4 = pygame.image.load("visuals/player_animation/player_run_3.png").convert_alpha()
 
-        frame4 = 
+        self.frames = [frame1,frame2,frame3,frame4]
+        self.frame_index = 0
+
+        self.pos = (250,750)
+
+    def curr_frame(self):
+        self.frame_index += 0.25
+        if self.frame_index >= len(self.frame_index):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.pos)
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] or keys[pygame._Left]:
+            if self.pos == (250,750):
+                self.pos = (84,750)
+
+            if self.pos == (84,750):
+                self.pos = self.pos
+            
+            if self.pos == (417,)
+        
+        
