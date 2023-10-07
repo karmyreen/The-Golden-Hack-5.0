921e14864b26a9476d2ed0f4fcfc23acc87d214a
August R.M <sjsaug@gmail.com>
2023-10-07 16:36:42 -0400

    cmt

--- ai.py
+++ ai.py
@@ -9,8 +9,9 @@ completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a video game character giving tips on how to improve personal hygiene."},
-    {"role": "user", "content": "Give the user 3 tips on how to improve personal hygiene."},
+    {"role": "user", "content": "Give the user 3 short tips in point form on how to improve personal hygiene."},
   ]
 )
 
-print(completion.choices[0].message)
+response = completion.choices[0].message
+print(response)
