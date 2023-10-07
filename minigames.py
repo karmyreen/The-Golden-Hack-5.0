b5d84d7f1a97d54ce8e7952d39cb485da91349b8
August R.M <sjsaug@gmail.com>
2023-10-07 19:44:50 -0400

    Co-authored-by: Zakariya <ZAKnCHEESE@users.noreply.github.com> Co-authored-by: karmyreen <karmyreen@users.noreply.github.com>

--- minigames.py
+++ minigames.py
@@ -1,11 +1,15 @@
 from settings import *
 import pygame
 
+from main import *
+
 
 
 bg = pygame.image.load("visuals/scary mouth.png").convert_alpha()
 
-bg.blit()
+
+
+
 
 
 
@@ -16,5 +20,6 @@ bg.blit()
 
 
 def brushingTeeth():
+    screen.blit(bg, (0,0))
 
     
