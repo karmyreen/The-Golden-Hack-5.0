f041c59965b7a2031a24b4503c8708ad935cc64d
August R.M <sjsaug@gmail.com>
2023-10-07 14:11:11 -0400

    Co-authored-by: ZAKnCHEESE <ZAKnCHEESE@users.noreply.github.com> Co-authored-by: karmyreen <karmyreen@users.noreply.github.com> Co-authored-by: Zakariya <ZAKnCHEESE@users.noreply.github.com>

--- loader.py
+++ loader.py
@@ -1,38 +1,21 @@
 import configparser
-def read_config():
-    configParser = configparser.RawConfigParser()
-    configFilePath = r'./codeguesser.cfg'
+
+configParser = configparser.RawConfigParser()
+
+def read_settings():
+    configFilePath = r'./settings.cfg'
     configParser.read(configFilePath)
-    fixes_options = dict(configParser.items('Fixes'))
-    dev_options = dict(configParser.items('Dev'))
     settings_options = dict(configParser.items('Settings'))
 
-    #FIXES
-    global is_iterm2
-    is_iterm2 = fixes_options['is_iterm2']
-
-    #DEV
-    global enable_debugging_check
-    global enable_debugging
-    enable_debugging_check = dev_options['enable_debugging']
-    if enable_debugging_check == "True":
-        enable_debugging = True
-    else:
-        enable_debugging = False
-
     #SETTINGS
-    
-    global start_screen_check
-    global start_screen
-    start_screen_check = settings_options['start_screen']
-    if start_screen_check == "True":
-        start_screen = True
 
-    global viewing_lenght
-    viewing_lenght = settings_options['viewing_timer_lenght']
+    WIDTH = settings_options['width']
+    HEIGHT = settings_options['height']
+    FPS = settings_options['fps']
 
-    global num_turns
-    num_turns = settings_options['number_of_turns']
+    return WIDTH, HEIGHT, FPS
 
-    global snippet_color
-    snippet_color = settings_options['snippet_text_color']
+def read_character():
+    configFilePath = r'./character.cfg'
+    configParser.read(configFilePath)
+    settings_options = dict(configParser.items('Settings'))
--- main.py
+++ main.py
@@ -2,9 +2,35 @@ import pygame, sys
 from settings import *
 
 class Game:
-    def __innit__(self):
+    def __init__(self):
+
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption('Hygiene Hero')
+        self.clock = pygame.time.Clock()
+    
+
+    def run(self):
+
+        while True:
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
+    
+
+
 
 
 
