35aa99f8fd3d46299440302af944a8a292b59218
August R.M <sjsaug@gmail.com>
2023-10-08 00:22:44 -0400

    Co-authored-by: Zakariya <ZAKnCHEESE@users.noreply.github.com> Co-authored-by: karmyreen <karmyreen@users.noreply.github.com>

--- enemies.py
+++ enemies.py
@@ -1,8 +1,42 @@
+from typing import Any
 import pygame
-from random import randint
+from random import *
 
-from pygame.sprite import _Group
 
 class Germs(pygame.sprite.Sprite):
-    def __init__(self, *groups: _Group) -> None:
-        super().__init__(*groups)
+    def __init__(self, type):
+        super().__init__()
+
+        red_frame_1 = pygame.image.load("visuals/red germs/red_germ_0.png").convert_alpha()
+        red_frame_2 = pygame.image.load("visuals/red germs/red_germ_1.png").convert_alpha()
+
+        yellow_frame_1 = pygame.image.load("visuals/yellow germs/yellow_germ_0.png").convert_alpha()
+        yellow_frame_2 = pygame.image.load("visuals/yellow germs/yellow_germ_1.png").convert_alpha()
+
+        if type == "red":
+            self.frames = [red_frame_1,red_frame_2]
+            self.speed = 10
+         
+        if type == "yellow":
+            self.frames = [yellow_frame_1,yellow_frame_2]
+            self.speed = 6
+        
+        self.frame_index = 0
+
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(center = (choice([84,250,417]),randint(-120,-20)))
+
+    def curr_frame(self):
+        self.frame_index += 0.20
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+    
+    def update(self):
+        self.curr_frame()
+        self.rect.y += self.speed
+        if self.rect.y > 860:
+            self.kill()
+            
+        return
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from loader import *
 from runner import Runner
+from enemies import Germs
+from random import randint, choice
 
 WIDTH, HEIGHT, FPS = read_settings()
 
@@ -20,16 +22,24 @@ import_assets()
 runner = pygame.sprite.GroupSingle()
 runner.add(Runner())
 
+germs_monsters = pygame.sprite.Group()
+
+timer = pygame.USEREVENT + 1
+pygame.time.set_timer(timer,900)
+
 scroll = 0
 
 while True:
-    screen.blit(background,(0,0))    
+    screen.blit(background,(0,0))
 
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        if event.type == timer:
+            germs_monsters.add(Germs(choice(["yellow","yellow","red"])))
 
     for i in range(0,3):
         screen.blit(background,(0,i * -835 + scroll))
@@ -40,6 +50,13 @@ while True:
 
     runner.draw(screen)
     runner.update()
+
+    germs_monsters.draw(screen)
+    germs_monsters.update()
+
+    if pygame.sprite.spritecollide(runner.sprite,germs_monsters,False):
+        pygame.quit()
+        sys.exit()
 
     pygame.display.flip()
     pygame.display.update()
