017901219a425e3845c59835cff6737825000c8e
August R.M <sjsaug@gmail.com>
2023-10-08 09:00:24 -0400

    fix rate limit

--- main.py
+++ main.py
@@ -37,12 +37,9 @@ pygame.time.set_timer(timer,900)
 
 scroll = 0
 
+#toothbrush_example1, toothbrush_example2, toothbrush_example3 = generate_ai("You are a video game character giving tips on how to brush your teeth effectively.", "Give the user 3 short tips in point form on how to brush their teeth effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
 toothbrush_example1, toothbrush_example2, toothbrush_example3 = generate_ai("You are a video game character giving tips on how to brush your teeth effectively.", "Give the user 3 short tips in point form on how to brush their teeth effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
-laundry_example1, laundry_example2, laundry_example3 = generate_ai("You are a video game character giving tips on how to do laundry effectively.", "Give the user 3 short tips in point form on how to do their laundry more effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
-handwashing_example1, handwashing_example2, handwashing_example3 = generate_ai("You are a video game character giving tips on how to wash hands effectively.", "Give the user 3 short tips in point form on how to wash hands effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
 false_toothbrush_tip1, false_toothbrush_tip2, false_toothbrush_tip3 = generate_ai("You are a video game character giving obviously false tips on how to brush their teeth effectively.", "Give the user 3 short obviously false tips in point form on how to brush their teeth more effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
-false_laundry_tip1, false_laundry_tip2, false_laundry_tip3 = generate_ai("You are a video game character giving obviously false tips on how to do laundry effectively.", "Give the user 3 short obviously false tips in point form on how to do their laundry more effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
-false_handwashing_tip1, false_handwashing_tip2, false_handwashing_tip3 = generate_ai("You are a video game character giving obviously false tips on how to wash hands effectively.", "Give the user 3 short obviously false tips in point form on how to wash hands more effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
 
 while True:
     screen.blit(background,(0,0))
