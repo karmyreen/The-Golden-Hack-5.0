925082efac13c6bebdf3cc73cda00c60be3d6b03
August R.M <sjsaug@gmail.com>
2023-10-08 11:18:24 -0400

    pre-demo commit Co-authored-by: karmyreen <karmyreen@users.noreply.github.com> Co-authored-by: Zakariya <ZAKnCHEESE@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from ai import generate_ai
 
 WIDTH, HEIGHT, FPS = read_settings()
 
+
 pygame.init()
+
+FONT_SIZE = 24
+font = pygame.font.SysFont(None, FONT_SIZE)
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Hygiene Hero")
@@ -22,7 +26,14 @@ def import_assets():
     global background
     background = pygame.image.load("visuals/background.png").convert_alpha()
     title_screen = pygame.image.load("visuals/hygienehero_title.png").convert_alpha()
+    
 
+def display_score():
+    global curr_time
+    curr_time = int((pygame.time.get_ticks()/100) - start_time) 
+    score_display = font.render(f"score {curr_time}",False,(0,0,0))
+    score_rect = score_display.get_rect(topleft = (20,50))
+    screen.blit(score_display,score_rect)
 
 import_assets()
 start_time = 0
@@ -33,14 +44,15 @@ runner.add(Runner())
 germs_monsters = pygame.sprite.Group()
 
 timer = pygame.USEREVENT + 1
-pygame.time.set_timer(timer,900)
+pygame.time.set_timer(timer,1100)
 
 scroll = 0
 
-hygiene_type_li = ["toothbrush","handwashing","laundry"]
-hygiene_type = choice(hygiene_type_li)
+
 
 def prompt_question():
+    hygiene_type_li = ["toothbrush","handwashing","laundry"]
+    hygiene_type = choice(hygiene_type_li)
     if hygiene_type == "toothbrush":
         if randint(0,1):   #true question
             question, _,_ = generate_ai("You are a video game character giving tips on how to brush your teeth effectively.", "Give the user 3 short tips in point form on how to brush their teeth effectively. Each tip should only be 10-15 words long. Don't include backslash n for new lines in the answer.")
@@ -85,7 +97,7 @@ while True:
         if game_state == "title":
             if keys[pygame.K_SPACE]:
                 game_state = "running"
-                start_time = int((pygame.time.get_ticks()/100))
+                germs_monsters.empty()
 
     if game_state == "running":
         for i in range(0,3):
@@ -101,9 +113,10 @@ while True:
         germs_monsters.draw(screen)
         germs_monsters.update()
 
-        curr_time = int((pygame.time.get_ticks()/100) - start_time) 
+        display_score()
         
         print(curr_time)
+
         if pygame.sprite.spritecollide(runner.sprite,germs_monsters,False):
             game_state = "quiz"
 
@@ -117,9 +130,70 @@ while True:
     if game_state == "quiz":
         #screen.fill((0,0,255))
         # Define the font and font size to use for the variable
-        FONT_SIZE = 24
-        font = pygame.font.SysFont(None, FONT_SIZE)
+        
+        true = pygame.image.load("visuals/image_50.png").convert_alpha()
+        false = pygame.image.load("visuals/image2_350x114.png").convert_alpha()
 
+
+        # Define the position of the true button
+        true_position = (250, 750)
+        false_position = (250, 620)
+        # Get the rectangle of the true button image
+        true_rect = true.get_rect(center=true_position)
+        false_rect = false.get_rect(center=false_position)
+        # Render the example variable as a Pygame surface
+        surface = font.render(question, True, (0, 0, 0), (255, 255, 255))
+
+        # Blit the example surface and the true button image onto the screen
+
+        screen.blit(surface, (0, 250))
+        screen.blit(true, true_rect) #350x128
+        screen.blit(false, false_rect) #350x114
+
+        mx,my = pygame.mouse.get_pos()
+        
+        click = pygame.mouse.get_pressed()
+        if (mx >= 75 and mx<=425) and ( my>=686 and my <=814):
+            if click[0]:
+                user_answer = True
+                print(user_answer)
+
+                if user_answer == answer:
+                    game_state = "title"
+                    question, answer = prompt_question()
+                    germs_monsters.empty()
+                    game_state = "title"
+                    germs_monsters.empty()
+                    game_state = "running"
+                    
+                    
+                else:
+                    germs_monsters.empty()
+                    game_state = "title"
+                    curr_time = int((pygame.time.get_ticks()/100))
+                    question, answer = prompt_question()
+                    germs_monsters.empty()
+
+        if (mx >= 75 and mx<=425) and (my >= 556 and my <=680):
+            if click[0]:
+                user_answer = False
+        
+                print(user_answer)
+
+                if user_answer == answer:
+                    game_state = "title"
+                    question, answer = prompt_question()
+                    germs_monsters.empty()
+                    game_state = "title"
+                    germs_monsters.empty()
+                    game_state = "running"
+
+                else:
+                    germs_monsters.empty()
+                    game_state = "title"
+                    curr_time = int((pygame.time.get_ticks()/100))
+                    question, answer = prompt_question()
+                    germs_monsters.empty()
         # Define the example variable
         
 
